48059a45fa0bf89176a81dad42929ea7e880203d
Unknown <hazik.jakub@gmail.com>
2017-08-07 15:15:13 +0200

    version v0.1: search date of name dialog, about dialog

--- src/script.py
+++ src/script.py
@@ -4,14 +4,11 @@ import gi
 import datetime
 import time
 
-
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk, AppIndicator3, GObject
 from threading import Thread
-
-#from pythonzenity import Message
-
+from gi.repository import GdkPixbuf
 
 class Indicator(Gtk.Window):
     def __init__(self):
@@ -20,7 +17,7 @@ class Indicator(Gtk.Window):
         self.names = Names("/home/jakub/Dropbox/Projects/Meniny_v_liste/Name-day-indicator/src/sk-meniny.csv")
 
         self.app = 'Name\'s day'
-        iconpath = "/home/jakub/Dropbox/Projects/Meniny_v_liste/Name-day-indicator/src/icon.ico"
+        iconpath = "/home/jakub/Dropbox/Projects/Meniny_v_liste/Name-day-indicator/src/calendar.png"
         self.indicator = AppIndicator3.Indicator.new(
             self.app, iconpath,
             AppIndicator3.IndicatorCategory.OTHER)
@@ -45,24 +42,6 @@ class Indicator(Gtk.Window):
             )
             self.indicator.set_menu(self.create_menu())
 
-    def about_message(self, widget):
-        #dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,Gtk.ButtonsType.OK, "Name day widget")
-        #Gtk.AboutDialog()
-        #dialog.format_secondary_text("You mustn\'t forget to friends and their name day! This widget help you with it.\n"
-        #                             u"\u00A9" + "Created by HAZman")
-        #dialog.run()
-        #dialog.destroy()
-        about = Gtk.AboutDialog()
-        about.set_program_name("Battery")
-        about.set_version("0.1")
-        about.set_copyright(u"\u00A9"+" Jan Bodnar")
-        about.set_comments("Battery is a simple tool for battery checking")
-        about.set_website("http://www.zetcode.com")
-        #about.set_logo(Gtk.Window.set_icon("/home/jakub/Dropbox/Projects/Meniny_v_liste/Name-day-indicator/src/calendar.png"))
-        about.set_logo(gtk.gdk.pixbuf_new_from_file( "calendar.png"))
-        about.run()
-        about.destroy()
-
     def create_menu(self):
         menu = Gtk.Menu()
         #today name
@@ -85,7 +64,7 @@ class Indicator(Gtk.Window):
         menu.append(menu_sep)
         # about dialog
         item_search = Gtk.MenuItem('Search')
-        item_search.connect('activate', self.search_dialog)
+        item_search.connect('activate', self.dialog_search)
         menu.append(item_search)
 
         # separator
@@ -93,7 +72,7 @@ class Indicator(Gtk.Window):
         menu.append(menu_sep)
         #about dialog
         item_about = Gtk.MenuItem('About')
-        item_about.connect('activate', self.about_message)
+        item_about.connect('activate', self.dialog_about)
         menu.append(item_about)
 
         # separator
@@ -107,16 +86,31 @@ class Indicator(Gtk.Window):
         menu.show_all()
         return menu
 
-    def search_dialog(self, widget):
+    def stop(self, source):
+        Gtk.main_quit()
+
+    def dialog_about(self, widget):
+        about = Gtk.AboutDialog()
+        about.set_program_name("Name day widget")
+        about.set_version("version v0.1")
+        about.set_copyright(u"\u00A9"+" HAZman")
+        about.set_comments("You mustn\'t forget to friends and their name day!\n This widget help you with it.\n")
+        about.set_website("https://github.com/JakubHazik/Name-day-indicator")
+        about.set_logo(GdkPixbuf.Pixbuf.new_from_file_at_size(
+            "/home/jakub/Dropbox/Projects/Meniny_v_liste/Name-day-indicator/src/calendar.png", 64, 64))
+        about.run()
+        about.destroy()
+
+    def dialog_search(self, widget):
         self.set_title("Search name in calendar")
         self.set_default_size(250, 95)
-        #self.set_position()
+        self.set_position(Gtk.WindowPosition.CENTER)
         self.label = Gtk.Label("Enter name")
         self.entry = Gtk.Entry()
         self.btn_search = Gtk.Button("Search")
         self.btn_search.connect("clicked",self.search)
         self.btn_cancel = Gtk.Button(stock= Gtk.STOCK_CANCEL)
-        self.btn_cancel.connect("clicked", )
+        self.btn_cancel.connect("clicked", self.dialog_close)
 
         fixed = Gtk.Fixed()
         fixed.put(self.label, 85, 5)
@@ -126,17 +120,22 @@ class Indicator(Gtk.Window):
 
         self.add(fixed)
         self.show_all()
-        #time.sleep(10)
-        #self.destroy()
 
-
+    def dialog_close(self,widget):
+        self.destroy()
 
     def search(self, widget):
-        print self.names.search_name(self.entry.get_text())
-        #print "hello", self.entry.get_text()
+        text = self.names.search_name(self.entry.get_text())
+        self.dialog_message("INFO", text)
+        self.destroy()
 
-    def stop(self, source):
-        Gtk.main_quit()
+    def dialog_message(self,type,text):
+        if type=="INFO":
+            md = Gtk.MessageDialog(self,
+                                   0, Gtk.MessageType.INFO,
+                                   Gtk.ButtonsType.OK, text)
+            md.run()
+            md.destroy()
 
 class Names():
     listNames = []
@@ -162,20 +161,17 @@ class Names():
 
     def search_name(self,name):
         year= datetime.date(datetime.date.today().year,1,1)
-        delta= datetime.timedelta(days=self.listNames.index(name)-1)
-        return year+delta
+        try:
+            days = self.listNames.index(name) - 1
+        except:
+            return "Name didn\'t find."
+        delta= datetime.timedelta(days)
+        date = year+delta
+        return date.strftime("%d. %B %Y \n%A")
 
     def gap_year(self, year):
         return (((year % 4 == 0) & (year % 100 != 0)) | (year % 400 == 0))
 
-
 indicator=Indicator()
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 Gtk.main()
-
-
-
-
-names=Names("/home/jakub/Dropbox/Projects/Meniny_v_liste/Name-day-indicator/src/sk-meniny.csv")
-#print names.search_name("Jo")
-# print names.today_name().strip('"\'')
